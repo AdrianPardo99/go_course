400464b1f777666410c7851e444d363a95ecdb26
Adrian González Pardo <gozapaadr@gmail.com>
2020-12-28 23:48:48 -0600

    Scanf ex

---
+++ flujos/sca.go
@@ -0,0 +1,8 @@
+package main
+import "fmt"
+func main(){
+  fmt.Println("Ingresa un valor entero")
+  var entero int
+  fmt.Scanf("%d",&entero)
+  fmt.Println("El valor entero ingresado es: ",entero)
+}
